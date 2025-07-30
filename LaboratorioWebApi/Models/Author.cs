55388bc03b7f9e37dc3baee2576f6b6eeb302000
Fernando Gazzana <fernando.gazzana@gmail.com>
2025-07-30 14:46:10 -0300

    Add domain models

---
+++ LaboratorioWebApi/Models/Author.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LaboratorioWebApi.Models;
+
+public class Author
+{
+    public Guid Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
