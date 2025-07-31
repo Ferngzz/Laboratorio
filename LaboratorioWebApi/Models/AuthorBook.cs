1da183b7d535821ccf3bb02f2896df6994ed356a
Fernando Gazzana <fernando.gazzana@gmail.com>
2025-07-31 13:40:32 -0300

    Feature/data (#7)
    * Updating bd access and models
    
    * Changes in models

--- LaboratorioWebApi/Models/AuthorBook.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace LaboratorioWebApi.Models;
-
-public class AuthorBook
-{
-    public Guid AuthorId { get; set; }
-    public Guid BookId { get; set; }
-}
--- LaboratorioWebApi/Models/Book.cs
+++ LaboratorioWebApi/Models/Book.cs
@@ -2,8 +2,9 @@
 
 public class Book
 {
-    public Guid Id { get; set; }
+    public Guid BookId { get; set; }
     public string Title { get; set; }
     public string Description { get; set; }
     public virtual List<Loan> Loans { get; set; }
+    public virtual List<Author> Authors { get; set; }
 }
